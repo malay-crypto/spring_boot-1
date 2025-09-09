ae1d2bc0fe5105c16e8aa4dc3f0345d1a1e1e012
malay <malay9836520821@gmail.com>
2025-09-09 23:29:15 +0530

    cross origin changed

--- src/main/java/com/example/demo8/controller/MyController.java
+++ src/main/java/com/example/demo8/controller/MyController.java
@@ -14,7 +14,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://react-spring-1.onrender.com")
+//https://react-spring-1.onrender.com
 public class MyController {
 
     @Autowired
